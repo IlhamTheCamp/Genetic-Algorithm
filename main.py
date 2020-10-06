96e35bc574c92ae122d1017683b9639950d4abe2
Ilham Rizki Hidayat <ilkuntwentyfive@gmail.com>
2020-10-06 08:46:35 +0700

    Decode

--- main.py
+++ main.py
@@ -1,51 +1,56 @@
 # Python3 program to create target string, starting from
 # random string using Genetic Algorithm
 
-import numpy as np
 import random
 import math
 
-def decode_chromosome(chromosome):
+def create_kromosom(banyakKromosom):
+    kromosom = []
+    for x in range(banyakKromosom):
+        kromosom.append(random.randint(0,1))
+    return kromosom
 
-    c1 = int(chromosome/2)
-    dc1 = np.zeros(chromosome)
-    dc1[0: c1] = 1
-    np.random.shuffle(dc1)
-    return(dc1)
+def create_populasi(banyakPopulasi):
+    populasi = []
+    for x in range(banyakPopulasi):
+        populasi.append(create_kromosom(10))
+    return populasi
 
-def create_population(individu, pchromosome):
-    population = np.zeros((individu, pchromosome))
-    for p in range(individu):
-        #random angka
-        pop = random.randint(0, pchromosome)
-        population[p, 0:pop] = 1
-        np.random.shuffle(population[p])
-    return(population)
+def decode_kromosom(kromosom):
+    x1 = -1 + ((3 / (2**-1 + 2**-2 + 2**-3 + 2**-4 + 2**-5)) * ((kromosom[0]*2**-1)+(kromosom[1]*2**-2)+(kromosom[2]*2**-3)+(kromosom[3]*2**-4)+(kromosom[4]*2**-5)))
+    x2 = -1 + ((2 / (2**-1 + 2**-2 + 2**-3 + 2**-4 + 2**-5)) * ((kromosom[5]*2**-1)+(kromosom[6]*2**-2)+(kromosom[7]*2**-3)+(kromosom[8]*2**-4)+(kromosom[9]*2**-5)))
+    return [x1,x2]
 
-def func(x1, x2):
-    f_hasil = 0
-    if x1 >= -1 and x1 <= 2 and x2 >= 1 and x2 <= 1 :
-        f_hasil = np.cos(x1)*np.sin(x2) - (x1 / ((x2 ** 2) + 1))
+def calculate_fitness(kromosom):
+    k = decode_kromosom(kromosom)
+    fitness = -1 * ((math.cos(k[0]) * math.sin(k[1])) - (k[0] / (k[1]**2 + 1)))
+    return fitness
 
-    return(f_hasil)
-
-def fitness(func, x1, x2):
-    #nilai a untuk menghindari pembagian dengan nol
-    a = 0.2
-
-    h = func(x1, x2)
-    f = 1 / (h + a)
-    return(f)
+def tournament_parent(pop, banyakPopulasi, banyakTourney):
+    best = []
+    for x in range(banyakTourney):
+        kromosom = pop[random.randint(1, banyakPopulasi)]
+        if (best == []) or calculate_fitness(kromosom) > calculate_fitness(best):
+            best = kromosom
+    return best
 
 def main():
-    chromosome = decode_chromosome(20)
-    print ("Hasil Chromosome: \n", chromosome)
-    population = create_population(4, 20)
-    print("\nHasil Populasi: \n", population)
-    hasil_fitness = fitness(func, chromosome, population)
-    print("\n Fitness Terbaik dari fungsi yang diberikan: \n",hasil_fitness)
+    # Tanya kenapa ada fungsi fitnessAll
+    # Tanya maksud dari ukuran Tourney
+    banyakPopulasi = 100
+    banyakTourney = 10
 
+    populasi = create_populasi(banyakPopulasi)
+    print("Contoh Populasi : ", populasi)
+    print("Hasil Dekode : ", decode_kromosom(populasi[2]))
+    print("Hasil Fitness : ", calculate_fitness(populasi[2]))
 
+    #tesTournament
+    parent1 = tournament_parent(populasi, banyakPopulasi, banyakTourney)
+    parent2 = tournament_parent(populasi, banyakPopulasi, banyakTourney)
+    print("Kromosom Orang Tua : ")
+    print(parent1)
+    print(parent2)
 
 if __name__ == '__main__':
     main()
